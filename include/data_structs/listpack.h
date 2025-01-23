b556cbd00782bf26bbacf75ccbd59567edd177f4
chenmiao <chenmiao.ku@gmail.com>
2025-01-23 08:50:48 +0000

    [feat :sparkles:]: unfinished listpack

--- include/data_structs/listpack.h
+++ include/data_structs/listpack.h
@@ -16,6 +16,14 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <optional>
+
+using listpack = uint8_t*;
+
+constexpr const size_t lp_hdr_size = 6;  // 4 bytes total size
+                                         // 2 bytes element size
+constexpr const size_t lp_eof_size = 1;  // 1bytes eof
+constexpr const size_t lp_eof      = 0xFF;
 
 typedef struct {
   uint8_t* sval;
@@ -23,93 +31,14 @@ typedef struct {
   uint64_t lval;
 } listpackEntry;
 
-unsigned char* lpNew( size_t capacity );
-void           lpFree( unsigned char* lp );
-unsigned char* lpShrinkToFit( unsigned char* lp );
-unsigned char* lpInsertString( unsigned char*  lp,
-                               unsigned char*  s,
-                               uint32_t        slen,
-                               unsigned char*  p,
-                               int             where,
-                               unsigned char** newp );
-unsigned char* lpInsertInteger( unsigned char*  lp,
-                                long long       lval,
-                                unsigned char*  p,
-                                int             where,
-                                unsigned char** newp );
-unsigned char* lpPrepend( unsigned char* lp, unsigned char* s, uint32_t slen );
-unsigned char* lpPrependInteger( unsigned char* lp, long long lval );
-unsigned char* lpAppend( unsigned char* lp, unsigned char* s, uint32_t slen );
-unsigned char* lpAppendInteger( unsigned char* lp, long long lval );
-unsigned char* lpReplace( unsigned char* lp, unsigned char** p, unsigned char* s, uint32_t slen );
-unsigned char* lpReplaceInteger( unsigned char* lp, unsigned char** p, long long lval );
-unsigned char* lpDelete( unsigned char* lp, unsigned char* p, unsigned char** newp );
-unsigned char* lpDeleteRangeWithEntry( unsigned char* lp, unsigned char** p, unsigned long num );
-unsigned char* lpDeleteRange( unsigned char* lp, long index, unsigned long num );
-unsigned char* lpBatchAppend( unsigned char* lp, listpackEntry* entries, unsigned long len );
-unsigned char* lpBatchInsert( unsigned char*  lp,
-                              unsigned char*  p,
-                              int             where,
-                              listpackEntry*  entries,
-                              unsigned int    len,
-                              unsigned char** newp );
-unsigned char* lpBatchDelete( unsigned char* lp, unsigned char** ps, unsigned long count );
-unsigned char* lpMerge( unsigned char** first, unsigned char** second );
-unsigned char* lpDup( unsigned char* lp );
-unsigned long  lpLength( unsigned char* lp );
-unsigned char* lpGet( unsigned char* p, int64_t* count, unsigned char* intbuf );
-unsigned char* lpGetValue( unsigned char* p, unsigned int* slen, long long* lval );
-int            lpGetIntegerValue( unsigned char* p, long long* lval );
-unsigned char*
-lpFind( unsigned char* lp, unsigned char* p, unsigned char* s, uint32_t slen, unsigned int skip );
-typedef int ( *lpCmp )( const unsigned char* lp,
-                        unsigned char*       p,
-                        void*                user,
-                        unsigned char*       s,
-                        long long            slen );
-unsigned char*
-lpFindCb( unsigned char* lp, unsigned char* p, void* user, lpCmp cmp, unsigned int skip );
-unsigned char* lpFirst( unsigned char* lp );
-unsigned char* lpLast( unsigned char* lp );
-unsigned char* lpNext( unsigned char* lp, unsigned char* p );
-unsigned char* lpPrev( unsigned char* lp, unsigned char* p );
-size_t         lpBytes( unsigned char* lp );
-size_t         lpEntrySizeInteger( long long lval );
-size_t         lpEstimateBytesRepeatedInteger( long long lval, unsigned long rep );
-unsigned char* lpSeek( unsigned char* lp, long index );
-typedef int ( *listpackValidateEntryCB )( unsigned char* p,
-                                          unsigned int   head_count,
-                                          void*          userdata );
-int            lpValidateIntegrity( unsigned char*          lp,
-                                    size_t                  size,
-                                    int                     deep,
-                                    listpackValidateEntryCB entry_cb,
-                                    void*                   cb_userdata );
-unsigned char* lpValidateFirst( unsigned char* lp );
-int            lpValidateNext( unsigned char* lp, unsigned char** pp, size_t lpbytes );
-unsigned int   lpCompare( unsigned char* p, unsigned char* s, uint32_t slen );
-void           lpRandomPair( unsigned char* lp,
-                             unsigned long  total_count,
-                             listpackEntry* key,
-                             listpackEntry* val,
-                             int            tuple_len );
-void           lpRandomPairs( unsigned char* lp,
-                              unsigned int   count,
-                              listpackEntry* keys,
-                              listpackEntry* vals,
-                              int            tuple_len );
-unsigned int   lpRandomPairsUnique( unsigned char* lp,
-                                    unsigned int   count,
-                                    listpackEntry* keys,
-                                    listpackEntry* vals,
-                                    int            tuple_len );
-void           lpRandomEntries( unsigned char* lp, unsigned int count, listpackEntry* entries );
-unsigned char* lpNextRandom( unsigned char* lp,
-                             unsigned char* p,
-                             unsigned int*  index,
-                             unsigned int   remaining,
-                             int            tuple_len );
-int            lpSafeToAdd( unsigned char* lp, size_t add );
-void           lpRepr( unsigned char* lp );
+enum class insert_type : uint8_t {
+  lp_before  = 0,
+  lp_after   = 1,
+  lp_replace = 2,
+};
+
+std::optional< listpack > lpNew( size_t capacity );
+constexpr void            lpFree( listpack lp );
+std::optional< listpack > lpShrinkToFit( listpack lp );
 
 #endif  //! REDISCC_DATA_STRUCTURE_LIST_PACK_H
